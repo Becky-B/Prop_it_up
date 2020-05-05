ecb558de4a63c16f0830a3067733b05deb4cd886
Becky-B <bettsrebecca@yahoo.com>
2020-05-05 16:36:49 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import './App.css';
+
+import PersonCard from "./Components/PersonCard";
+
+function App() {
+  return (
+    <div classname="App">
+      <PersonCard firstname ="Jane" lastname = "Doe" age = {45} hairColor = "Black"/>
+      <PersonCard firstname ="John" lastname = "Smith" age = {88} hairColor = "Brown"/>
+      <PersonCard firstname ="Millorde" lastname = "Fillmore" age = {50} hairColor = "Brown "/>
+      <PersonCard firstname ="Maria" lastname = "Smith" age = {62} hairColor = "Brown"/>
+    </div>
+  );
+}
+
+export default App;
